a02c1e7d2c1772907ca2d2a9dc6d2c6f112fd5f0
Manideep1308 <manideep.sankabathula@innoart.in>
2022-03-31 16:26:44 +0530

    added shell

---
+++ shell.sh
@@ -0,0 +1,51 @@
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+
+apt-cache madison docker-ce
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+#install docker
+sudo apt install docker.io
+
+
+#install docker compose
+sudo apt install docker-compose
+
+
+#check version
+docker --version
+
+#check compose version
+docker-compose --version
+
+
+#automating the compose
+
+echo "docker running containers"
+docker ps
+
+
+
+echo "start docker compose"
+docker-compose up -d
+
+
+echo "now running containers"
+docker ps
+
+
